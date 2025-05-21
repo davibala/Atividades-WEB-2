62fdfcddac97d84d83d4c331897c85818a418543
davibala <davidsantosdesouza777@gmail.com>
2025-05-21 19:50:57 -0300

    .

--- atividade_5.2/resources/views/layouts/app.blade.php
+++ atividade_5.2/resources/views/layouts/app.blade.php
@@ -15,6 +15,10 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <!-- Libraries -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
